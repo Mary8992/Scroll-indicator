01908694581700025965848d523322c22a179246
Mary8992 <mc046569@gmail.com>
2023-03-13 23:18:56 +0300

    vertical scroll bar and some changes added

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
-const progress = document.querySelector('.progress-bar');
+const progressH = document.querySelector('.progress-bar');
+const progressV = document.getElementById('progressbar');
 
-window.addEventListener('scroll', ()=>{
+//horizontal
+window.addEventListener('scroll', () => {
     //we found our position in the viewport
     const winScroll = window.pageYOffset;
    //get total height
     const height = document.documentElement.scrollHeight - window.innerHeight;
     const scrolled = winScroll / height * 100;
 
-    progress.style.width = `${scrolled}%`
+    progressH.style.width = `${scrolled}%`
 })
+
+//vertical
+let totalHeight = document.body.scrollHeight - window.innerHeight;
+window.onscroll = function() {
+    let progressHeight = (window.pageYOffset / totalHeight) * 100;
+    progressV.style.height = progressHeight + '%'
+}
